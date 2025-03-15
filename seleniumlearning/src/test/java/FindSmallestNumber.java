402628907a01a56c73961f5694ce798bd5eff226
Kavindu Maleesha <91423119+Kavimaleesha@users.noreply.github.com>
2025-03-15 21:40:33 +0530

    Find Smallest Number

---
+++ seleniumlearning/src/test/java/FindSmallestNumber.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class FindSmallestNumber {
+
+
+        Integer givenArray[] = {2,1,5,3,4};
+
+
+        //1st approach
+        public void findSmallestValue(){
+            int smallest = Integer.MAX_VALUE;
+
+            for(int i = 0; i < givenArray.length ; i++){
+                if (givenArray[i]<smallest){
+                    smallest = givenArray[i];
+
+                }
+            }
+            System.out.println(smallest);
+        }
+
+    //2nd approach
+        public void usingArrays(){
+            Arrays.sort(givenArray);
+            System.out.println(givenArray[0]);
+    }
+
+    //3rd approach
+            public void usingCollections(){
+                List<Integer> list = Arrays.asList(givenArray);
+                Collections.sort(list);
+                int smallest = list.get(0);
+                System.out.println(smallest);
+    }
+
+        public static void main(String[] args) {
+            FindSmallestNumber small = new FindSmallestNumber();
+            //small.findSmallestValue();
+            //small.usingArrays();
+            small.usingCollections();
+    }
+}
